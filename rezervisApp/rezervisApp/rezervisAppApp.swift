2132abe53dcd51097d3278539865fa509d447b3d
oktaydin <oktayaydin3437@gmail.com>
2025-03-07 12:56:44 +0300

    Rezervis
    Mobil için geliştirilen uygulama.

---
+++ rezervisApp/rezervisApp/rezervisAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  rezervisAppApp.swift
+//  rezervisApp
+//
+//  Created by Oktay Aydın on 7.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct rezervisAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
